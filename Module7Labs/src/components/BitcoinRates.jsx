206a104cd8fdce7525743775dc278d80914ca8e3
Ctbenoit60 <ctbenoit60@gmail.com>
2024-01-12 18:21:28 -0600

    added a toLowerCase

--- Module7Labs/src/components/BitcoinRates.jsx
+++ Module7Labs/src/components/BitcoinRates.jsx
@@ -7,19 +7,34 @@ function BitcoinRates() {
   const [bitcoinPrice, setBitcoinPrice] = useState(null);
 
   useEffect(() => {
+    console.log("running Effect");
     const fetchBitcoinPrice = async () => {
       try {
         const response = await fetch(
           `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}`
         );
-        const data = await response.json();
-        setBitcoinPrice(data.bitcoin[currency]);
+        const data = await response.json(); // fetch data => json object
+        console.log(data, "Json object"); // shows json object and string to mark json object converted above
+        //notice that if you expand the {bitcoin: {...}} it will drop down and the next data will show "bitcoin: {usd: 42743}"
+
+        //case sensitivity?????
+
+        setBitcoinPrice(data.bitcoin[currency.toLowerCase()]); //adding lowerCase method to test case sensitivity
+        // console.log(
+        //   "Bitcoin price for",
+        //   currency,
+        //   "is",
+        //   data.bitcoin[currency.toLowerCase()]
+        // ); //need to add here as well to render value in console.
+        // console.log(bitcoinPrice, "current bitcoinPrice value");
       } catch (error) {
         console.error("Error fetching Bitcoin price:", error);
       }
     };
 
     fetchBitcoinPrice();
+
+    console.log(bitcoinPrice);
 
     // Cleanup function to cancel any ongoing requests or subscriptions
     return () => {
